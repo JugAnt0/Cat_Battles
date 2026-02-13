4283335a422a374905e7883ddf23f320d79184fb
JugAnt0 <canaletwomeyantonio@gmail.com>
2026-02-13 17:52:24 -0500

    Remove paka's-petting-sim folder

--- paka's-petting-sim/script/camera_2d.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-@export var smooth_speed := 8.0  
-
-func _process(delta: float) -> void:
-	var target_pos = get_global_mouse_position()
-	global_position = global_position.lerp(target_pos, delta * smooth_speed)
--- paka's-petting-sim/script/shany's_camera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-@onready var anim: AnimationPlayer = $AnimationPlayer
-
-
-
-func _ready():
-	anim.play("intro_zoom")
