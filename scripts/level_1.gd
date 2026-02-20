046215b19bf67597e5d98adc2197c50c407cf18f
JugAnt0 <canaletwomeyantonio@gmail.com>
2026-02-20 13:30:51 -0500

    Finished level 1 (Added victory flag, world border and trees)

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,4 +1,8 @@
 extends Node2D
+@onready var panda: CharacterBody2D = $Panda
+@onready var timer: Timer = $Timer
+@onready var death_timer: Timer = $kill_zone/Death_Timer
+@onready var camera: Camera2D = $Panda/Camera
 
 @export var level_number:int = 1
 @onready var player: AnimationPlayer = $Panda/Camera/AnimationPlayer
@@ -15,3 +19,26 @@ func _on_level_completed():
 	if Progress.unlocked_level <= level_number:
 		Progress.unlocked_level = level_number + 1
 		Progress.save()
+
+
+func _on_flag_body_entered(body: Node2D) -> void:
+	if body == panda:
+		unlock_next_level()
+		player.play("zoom_out")
+		timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+
+func _on_kill_zone_body_entered(body: Node2D) -> void:
+	if body == panda:
+		player.play("zoom_out")
+		death_timer.start()
+		body.get_node("CollisionShape2D").queue_free()
+
+
+func _on_death_timer_timeout() -> void:
+	get_tree().reload_current_scene()
